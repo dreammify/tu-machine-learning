b2ae274e49eda16ae2b1653c6ad688efca37dac4
PanosIs <panoslantavos@gmail.com>
2020-04-27 20:15:21 +0300

    Create generate_idf_dataset function

--- src/data/idf_dataset.py
+++ src/data/idf_dataset.py
@@ -1,2 +1,13 @@
-import requests
-import matplotlib
+import pandas
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def generate_idf_vectors(dataframe: pandas.DataFrame, mask, column: str):
+    train_frame = dataframe[mask]
+    test_frame = dataframe[~mask]
+
+    tokenizer = TfidfVectorizer(strip_accents='unicode', min_df=10)
+    tokenizer.fit(train_frame[column])
+
+    train_tokens = tokenizer.transform(train_frame[column])
+    test_tokens = tokenizer.transform(test_frame[column])
