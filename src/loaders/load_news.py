558a8e388045d92fe1d3181d588a1e0e690b061a
PanosIs <panoslantavos@gmail.com>
2020-04-27 20:19:20 +0300

    Generate TF-IDF vectors from news dataset

--- src/loaders/load_news.py
+++ src/loaders/load_news.py
@@ -3,6 +3,8 @@ import pandas
 from typing import List
 from pandas import DataFrame
 
+from src.data.idf_dataset import generate_idf_vectors
+
 pandas.set_option('display.max_rows', 500)
 pandas.set_option('display.max_columns', 500)
 pandas.set_option('display.width', 1000)
@@ -30,8 +32,17 @@ def load_news_frame_pop(filename: str, popfilenames: List[str]):
                 merged = pandas.merge(data[basecolumns], popdata, on=["IDLink"], how="left")
                 data.fillna(merged)
 
-        data = data.dropna().sort_values(by="IDLink")
+        data = data.dropna().sort_values(by="IDLink").reset_index()
         return data
 
 if __name__ == "__main__":
-    load_news_frame_pop(filename = "resources/news.csv", popfilenames=["resources/news-fb-timeseries-economy.csv", "resources/news-fb-timeseries-microsoft.csv", "resources/news-fb-timeseries-obama.csv", "resources/news-fb-timeseries-palestine.csv"])
+    print("Loading dataset")
+    data = load_news_frame_pop(filename = "resources/news.csv", popfilenames=["resources/news-fb-timeseries-economy.csv", "resources/news-fb-timeseries-microsoft.csv", "resources/news-fb-timeseries-obama.csv", "resources/news-fb-timeseries-palestine.csv"])
+
+    print("Generating train/test mask")
+    mask = numpy.random.rand(len(data)) < 0.8
+
+    print("Performing TF-IDF on dataset")
+    train, test = generate_idf_vectors(data, mask, 'Title')
+    print(train.shape)
+    print(test.shape)
