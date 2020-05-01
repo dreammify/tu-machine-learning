45d7b19440dc235541818d2b18a44639f555919e
choffma9 <60776123+choffma9@users.noreply.github.com>
2020-05-01 19:10:37 +0200

    Add files via upload

---
+++ src/loaders/preprocessing_realestate.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 22 08:41:51 2020
+
+@author: Charlotte
+"""
+
+from pandas import read_excel
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from matplotlib import pyplot
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+
+#Finding zones with latitude and longitude
+#how many clusters? ----> 4
+def elbowcurve(data):
+    Nc = range(1, 10)
+    kmeans = [KMeans(n_clusters=i) for i in Nc]
+    kmeans
+    score = [kmeans[i].fit(data).score(data) for i in range(len(kmeans))]
+    score
+    pyplot.plot(Nc,score)
+    pyplot.xlabel('Number of Clusters')
+    pyplot.ylabel('Score')
+    pyplot.title('Elbow Curve')
+    pyplot.show()
+
+#Do clustering
+def clustering(data):
+    model = KMeans(n_clusters = 4)
+    model.fit(x)
+    model.labels_
+    colormap = np.array(['Red', 'Blue', 'Green','Orange'])
+    z = pyplot.scatter(x['X6 longitude'],x['X5 latitude'], c = colormap[model.labels_])
+    z
+    return model.labels_
+
+#one hot encoding of clusters
+def onehot_encoding(m):
+    onehot_encoder = OneHotEncoder(sparse=False)
+    m = m.reshape(len(m), 1)
+    onehot_encoded = onehot_encoder.fit_transform(m)
+    return onehot_encoded
+
+
+if __name__ == "__main__":
+    # Load dataset
+    dataset= read_excel('resources/Real_estate_valuation_data_set.xlsx')
+
+    x=dataset[['X5 latitude','X6 longitude']]
+    
+    elbowcurve(x)
+
+    #One hot encoding of clusters
+    m=clustering(x)
+    onehot_encoded = onehot_encoding(m)
+
+    #Splitting the dataset into Training set and Test Set
+    X=dataset.iloc[:, 1:-3].values
+    X=np.append(X,onehot_encoded,axis=1)
+    y=dataset.iloc[:, 7].values
+
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state= 0)
+
+    #Standardisation
+    std= StandardScaler()
+    x_train= std.fit_transform(x_train)
+    x_test=std.transform(x_test)
+
+
+
