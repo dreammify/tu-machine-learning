d8c36c3a7a755ac9dbc2bf9402478922bd2ccd34
PanosIs <panoslantavos@gmail.com>
2020-05-03 22:31:23 +0300

    Add day/severity heatmap visualization

--- src/loaders/visualize_accidents.py
+++ src/loaders/visualize_accidents.py
@@ -3,12 +3,32 @@ import numpy as np
 import pandas
 import seaborn as sns
 from pandas import DataFrame
+from sklearn import preprocessing
 
 pandas.set_option('display.max_rows', 500)
 pandas.set_option('display.max_columns', 500)
 pandas.set_option('display.width', 1000)
 
 def visualize_day_severity(frame: DataFrame):
+    temp_frame = frame[['Day_of_Week', 'Accident_Severity']]
+    temp_frame['Accident_Severity'] = temp_frame['Accident_Severity'].astype("int32")
+    temp_frame['Day_of_Week'] = temp_frame['Day_of_Week'].astype("int32")
+    temp_frame = temp_frame.groupby(['Day_of_Week', 'Accident_Severity'])\
+        .size().reset_index().rename(columns={0:'count'})
+
+    # Scaling, comment this out to get raw results
+    x = temp_frame[['count']].values.astype(float)
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x_scaled = min_max_scaler.fit_transform(x)
+    temp_frame['count'] = x_scaled
+
+    temp_frame = temp_frame.pivot("Accident_Severity", "Day_of_Week", "count")
+    f, ax = plt.subplots(figsize=(9, 6))
+    sns.heatmap(temp_frame, annot=True,  linewidths=.5, ax=ax)
+    plt.show()
+
+
+def visualize_dist(frame: DataFrame):
     temp_frame = frame[['Day_of_Week', 'Accident_Severity']]
     temp_frame['Accident_Severity'] = temp_frame['Accident_Severity'].astype("int32")
     temp_frame['Day_of_Week'] = temp_frame['Day_of_Week'].astype("int32")
@@ -31,4 +51,5 @@ def load_accidents_frame(filename: str):
 
 if __name__ == "__main__":
     data = load_accidents_frame("resources/accidents_2005_to_2007.csv")
+    # visualize_dist(data)
     visualize_day_severity(data)
