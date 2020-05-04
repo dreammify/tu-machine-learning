41cde1f79c0efb1696d89437421df6cab02c5835
PanosIs <panoslantavos@gmail.com>
2020-05-05 00:20:52 +0300

    Move plot_depthoftree.py to utilities directory

---
+++ src/utilities/plot_depthoftree.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 19:49:33 2020
+
+@author: Charlotte
+"""
+
+
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import cross_val_score
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+# function for fitting trees of various depths on the training data using cross-validation
+def run_cross_validation_on_trees(X, y, tree_depths, cv=10):
+    cv_scores_list = []
+    cv_scores_std = []
+    cv_scores_mean = []
+    accuracy_scores = []
+    for depth in tree_depths:
+        tree_model = DecisionTreeRegressor(max_depth=depth)
+        clf = make_pipeline(StandardScaler(), tree_model)
+        cv_scores = cross_val_score(clf, X, y, cv=cv)
+        cv_scores_list.append(cv_scores)
+        cv_scores_mean.append(cv_scores.mean())
+        cv_scores_std.append(cv_scores.std())
+        accuracy_scores.append(tree_model.fit(X, y).score(X, y))
+    cv_scores_mean = np.array(cv_scores_mean)
+    cv_scores_std = np.array(cv_scores_std)
+    accuracy_scores = np.array(accuracy_scores)
+    return cv_scores_mean, cv_scores_std, accuracy_scores
+  
+# function for plotting cross-validation results
+def plot_cross_validation_on_trees(depths, cv_scores_mean, cv_scores_std, accuracy_scores, title):
+    fig, ax = plt.subplots(1,1, figsize=(15,5))
+    ax.plot(depths, cv_scores_mean, '-o', label='mean cross-validation accuracy', alpha=0.9)
+    ax.fill_between(depths, cv_scores_mean-2*cv_scores_std, cv_scores_mean+2*cv_scores_std, alpha=0.2)
+    ylim = plt.ylim()
+    ax.plot(depths, accuracy_scores, '-*', label='train accuracy', alpha=0.9)
+    ax.set_title(title, fontsize=16)
+    ax.set_xlabel('Tree depth', fontsize=14)
+    ax.set_ylabel('Accuracy', fontsize=14)
+    ax.set_ylim(ylim)
+    ax.set_xticks(depths)
+    ax.legend()
+
+
