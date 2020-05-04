3077074dcc0a48bd56acde137b4d55e495fa74d7
PanosIs <panoslantavos@gmail.com>
2020-05-05 02:28:10 +0300

    Crop the traffic dataset to 5000 items and use all regressions

--- src/traffic_parameter_search_entrypoint.py
+++ src/traffic_parameter_search_entrypoint.py
@@ -4,7 +4,7 @@ import src.loaders.preprocessing_traffic as traffic
 from src.regressors.factories import Hosts
 
 if __name__ == "__main__":
-    dataset = traffic.load_data()
+    dataset = traffic.load_data().head(5000)
     x_train, x_test, y_train, y_test = traffic.preprocess_data(dataset)
 
     host_list = [
@@ -13,7 +13,8 @@ if __name__ == "__main__":
         Hosts.kernel_ridge_host_gamma,
         Hosts.linear_svr_host_C,
         Hosts.gaussian_svr_host_C,
-        Hosts.gaussian_svr_host_d
+        Hosts.poly_svr_host_C,
+        Hosts.poly_svr_host_d
     ]
 
     def execute_host_search(host):
@@ -21,11 +22,11 @@ if __name__ == "__main__":
         host.do_test(x_test, y_test)
         host.plot_search("Accuracy per parameters for Traffic Volume")
 
-    with ThreadPoolExecutor(max_workers=2) as executor:
-        for host in host_list:
+    for host in host_list:
+        try:
+            execute_host_search(host)
+        except Exception as e:
+            print("HOST FAILED")
             print(host.regressor_factory)
-            try:
-                executor.submit(execute_host_search, host)
-            except Exception:
-                pass
+            print(e)
 
